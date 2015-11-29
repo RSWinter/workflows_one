f8bda83e681f4ef9f119d67a0d4d3deff2c76ad7
Rob Winter <info@winterdigital.com>
2015-11-29 12:32:14 -0500

    Added static file monitoring

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,8 @@ var jsSources = [
   'components/scripts/template.js'
 ];
 var sassSources = ['components/sass/style.scss'];
+var htmlSources = ['builds/development/*.html'];
+var jsonSources = ['builds/development/js/*.json'];
 
 gulp.task('coffee', function() {
   gulp.src(coffeeSources)
@@ -35,25 +37,37 @@ gulp.task('compass', function() {
     .pipe(compass({
       sass: 'components/sass',
       image: 'builds/development/images',
-      style: 'expanded',
-      // sourcemap: true
+      style: 'expanded'
+      //, sourcemap: true
     })
     .on('error', gutil.log))
     .pipe(gulp.dest('builds/development/css'))
     .pipe(connect.reload())
 });
 
-gulp.task("watch", function() {
+gulp.task('watch', function() {
   gulp.watch(coffeeSources, ['coffee']);
   gulp.watch(jsSources, ['js']);
   gulp.watch('components/sass/*.scss', ['compass']);
+  gulp.watch(htmlSources, ['html']);
+  gulp.watch(jsonSources, ['json']);
 });
 
 gulp.task('connect', function() {
   connect.server({
     root: 'builds/development/',
     livereload: true
-  })
+  });
 });
 
-gulp.task('default', ['coffee', 'js', 'compass', 'connect', 'watch']);
+gulp.task('html', function() {
+  gulp.src(htmlSources)
+  .pipe(connect.reload())
+});
+
+gulp.task('json', function() {
+  gulp.src(jsonSources)
+  .pipe(connect.reload())
+});
+
+gulp.task('default', ['html', 'json', 'coffee', 'js', 'compass', 'connect', 'watch']);
